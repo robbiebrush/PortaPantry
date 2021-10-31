78e87b79c8e7e2259340ccfe53087a79f6a6e5c1
Robbie Brush <W0741993@myscc.ca>
2021-10-30 23:54:04 -0400

    Created database classes:
    *add Database class
    *add database Constants class
    *add database Table Values class

--- src/DatabaseClasses/DBConsts.java
+++ src/DatabaseClasses/DBConsts.java
@@ -1,4 +1,7 @@
 package DatabaseClasses;
 
 public class DBConsts {
+    public static final String DB_NAME = "rbrushjava";
+    public static final String DB_USER = "rbrush";
+    public static final String DB_PASS = "44cqs44cqs4r0l34r0l3";
 }
