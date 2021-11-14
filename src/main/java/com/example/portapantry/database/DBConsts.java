e155ce908acd8aadfe436690cc8313521058bbc7
Robbie Brush <W0741993@myscc.ca>
2021-11-13 19:38:56 -0500

    Fixing .gitignore again, cleaning up directories

--- src/main/java/com/example/portapantry/database/DBConsts.java
+++
@@ -1,7 +0,0 @@
-package com.example.portapantry.database;
-
-public class DBConsts {
-    public static final String DB_NAME = "rbrushjava";
-    public static final String DB_USER = "rbrush";
-    public static final String DB_PASS = "44cqs44cqs4r0l34r0l3";
-}
