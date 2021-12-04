c5b327b0b57855a52abdc2157739f957830d8820
Robbie Brush <W0741993@myscc.ca>
2021-12-04 18:22:01 -0500

    Program successfully creates tables, as well as populates needed tables

--- src/main/java/com/example/portapantry/database/Database.java
+++ src/main/java/com/example/portapantry/database/Database.java
@@ -77,12 +77,31 @@ public class Database {
             createTable = connection.createStatement();
             createTable.execute(tableQuery);
             System.out.println("The " + tableName + " table has been inserted");
-            //Populate the food_groups table
-            //populateTable(TABLE_FOOD_GROUPS,
-                    //DBTableValues.POPULATE_TABLE_FOOD_GROUPS, connection);
-            //Populate the food_allergies table
-            //populateTable(TABLE_FOOD_ALLERGIES,
-                    //DBTableValues.POPULATE_TABLE_FOOD_ALLERGIES, connection);
+            if (tableName == "food_groups") {
+                //Populate the food_groups table
+                populateTable(TABLE_FOOD_GROUPS,
+                        DBTableValues.INSERT_GROUPS_PROTEINS, connection);
+                populateTable(TABLE_FOOD_GROUPS,
+                        DBTableValues.INSERT_GROUPS_GRAINS, connection);
+                populateTable(TABLE_FOOD_GROUPS,
+                        DBTableValues.INSERT_GROUPS_VEGS, connection);
+                populateTable(TABLE_FOOD_GROUPS,
+                        DBTableValues.INSERT_GROUPS_FRUITS, connection);
+                populateTable(TABLE_FOOD_GROUPS,
+                        DBTableValues.INSERT_GROUPS_DAIRY, connection);
+                populateTable(TABLE_FOOD_GROUPS,
+                        DBTableValues.INSERT_GROUPS_OTHER, connection);
+            } else if (tableName == "food_allergies") {
+                //Populate the food_allergies table
+                populateTable(TABLE_FOOD_ALLERGIES,
+                        DBTableValues.INSERT_ALLERGIES_NONE, connection);
+                populateTable(TABLE_FOOD_ALLERGIES,
+                        DBTableValues.INSERT_ALLERGIES_NUTS, connection);
+                populateTable(TABLE_FOOD_ALLERGIES,
+                        DBTableValues.INSERT_ALLERGIES_SEA_FOOD, connection);
+                populateTable(TABLE_FOOD_ALLERGIES,
+                        DBTableValues.INSERT_ALLERGIES_UNKNOWN, connection);
+            }
         }
     }
 
@@ -90,6 +109,6 @@ public class Database {
                                Connection connection) throws SQLException {
         Statement statement = connection.createStatement();
         statement.execute(query);
-        System.out.println("The " + tableName + " has been populated.");
+        System.out.println("Record added to " + tableName + ".");
     }
 }
